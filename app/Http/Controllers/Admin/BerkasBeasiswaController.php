b61cf5def955b0782e394be73423dd8dc9b622be
Jansfy_ <ibjansyarifhidayatullah240101@gmail.com>
2022-03-02 16:08:25 +0800

    nnn

--- app/Http/Controllers/Admin/BerkasBeasiswaController.php
+++ app/Http/Controllers/Admin/BerkasBeasiswaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\BerkasModel;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 
 class BerkasBeasiswaController extends Controller
 {
